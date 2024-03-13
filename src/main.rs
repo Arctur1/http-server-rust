3d6c4e66ab362523a9e561bb179a1cee5bf9e180
Aleksei Rozhnov <rozhnov.alexey@gmail.com>
2024-03-13 21:34:21 +0400

    add http response

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
-use std::net::TcpListener;
+use std::{io::{Read, Write}, net::{TcpListener, TcpStream}};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(stream) => {
+                handle_client(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_client(mut stream: TcpStream) {
+    stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").expect("writing to stream");
 }
